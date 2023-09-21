a612ee749ce696db15de91ac7c1a854c85f44cd1
Eduardo Mendes <eduardo.mendes@sensedia.com>
2023-09-21 08:02:58 -0300

    iniciando o projeto storybook

---
+++ src/componentes/AtBotaoTest/index.tsx
@@ -0,0 +1,25 @@
+/*PRIMEIRA ATIVIDADE*/
+
+// import React from "react";
+// import styled from "styled-components";
+
+// const BtEstilizadoTest = styled.button`
+//     background: green;
+//     padding: 16px 32px;
+//     border: 2px solid green;
+//     color: white;
+//     font-size: 20px;
+//     cursor:  pointer;
+//     &:hover{
+//         background: red;
+//         border:2px solid red;
+//     }
+// `
+
+// export const AtBotaoTest = () => {
+//     return (
+//         <BtEstilizadoTest>
+//             Clique aqui Test!
+//         </BtEstilizadoTest>
+//     )
+// }
---
+++ stories/AtBotao.stories.tsx
@@ -0,0 +1,15 @@
+/*PRIMEIRA ATIVIDADE*/
+
+// import React from 'react';
+// import {AtBotaoTest} from '../src'
+// import {ComponentMeta, ComponentStory } from '@storybook/react'
+
+// export default {
+//     title: 'Componentes/AtBotaoTest',
+//     component: AtBotaoTest
+// } as ComponentMeta<typeof AtBotaoTest>
+
+
+// const Template: ComponentStory<typeof AtBotaoTest> = () => <AtBotaoTest />
+
+// export const Segundario = Template.bind({})
